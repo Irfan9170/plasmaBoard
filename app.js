8db846d86a39e17cbcbc54136545efe2640dec7b
Irfan <irfanali987654@gmail.com>
2021-04-22 15:04:27 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const ejs = require("ejs");
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
 
