a51eb47977b448f788e46176bb982cdf10bfa5bb
Irfan <irfanali987654@gmail.com>
2021-04-28 00:20:55 +0530

    okk

--- app.js
+++ app.js
@@ -77,14 +77,48 @@ const volunteerSchema = new mongoose.Schema({
 
 const Volunteer = mongoose.model('Volunteer',volunteerSchema);
 
+const resourceSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:Number
+    },
+    verify:{
+        type:String
+    },
+    status:{
+        type:String
+    },
+    city:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    resource:{
+        type:String
+    }
+   
+   
+})
+
+const Resource = mongoose.model('Resource',resourceSchema);
+
 app.get('/', (req, res) => {
-    res.render("home");
+    Donor.find({},  function (err, person) {
+        
+        res.render("home",{person:person});
+        
+      });
+    
 })
 app.get('/donor',(req,res)=>{
     res.render("donor");
 })
 app.post('/donor',(req,res)=>{
-    console.log(req.body)
+    
     const donor = new Donor({
         name:req.body.name,
         mobile:req.body.mobile,
@@ -115,6 +149,36 @@ app.post('/search',(req,res)=>{
       
       });
    
+})
+app.get('/resources',(req,res)=>{
+    res.render("resources")
+})
+app.post('/resources',(req,res)=>{
+    const resource = new Resource({
+        name:req.body.name,
+        mobile:req.body.mobile,
+        verify:req.body.verify,
+        status:req.body.status,
+        city:req.body.city,
+        address:req.body.address,
+        resource:req.body.res
+    })
+    resource.save();
+    res.redirect("/")
+})
+// app.get('/resource',(req,res)=>{
+//     res.render("resource")
+// })
+app.post('/resource',(req,res)=>{
+    console.log(req.body)
+    Resource.find({'city':req.body.city,'resource':req.body.res}, function (err, resource) {
+        if (err) {
+            res.render("err")
+            console.log(err);
+        };
+        res.render("resource",{res:resource})
+      });
+    
 })
 app.post('/city',(req,res)=>{
         Donor.find({'city':req.body.city}, 'name mobile', function (err, p) {
